b5df94ad4a43239d861e227b9d45a6e145f81146
Andrea <andreakgd@gmail.com>
2015-07-14 20:06:07 +0100

    .

--- PowerShell/Update meta for all items.ps1
+++ PowerShell/Update meta for all items.ps1
@@ -1,16 +1,16 @@
-#Use this to update all items in a list or doc library
+# Use this to update all items in a list or doc library
 
-#Change the URL to the site you want
+# Change the URL to the site you want
 $web = Get-SPWeb http://site.com/subsite
 
 #Change the list name to the one you want
 $list = $web.lists["Resumes"]
 $items = $list.Items 
 
-#Change the SharePoint User ID to the one you want
+# Change the SharePoint User ID to the one you want
 $SPFieldUserValue = New-Object Microsoft.SharePoint.SPFieldUserValue ($SPWeb,2184)
 
-#Change the date to the one you want
+# Change the date to the one you want
 $dateToStore = Get-Date "10/02/2010"
 
 foreach ($item in $items) 
