293976eccfe15106db4f32185016b3abbe04a898
Andrea Goodson <5778606+akgd@users.noreply.github.com>
2018-04-27 12:06:11 +0100

    Prettify

--- addIdsToSpForm.js
+++ addIdsToSpForm.js
@@ -1,11 +1,11 @@
 //add IDs to SP form rows and cells
-	const spFormLabels = document.querySelectorAll('.ms-formlabel');
-	for (var i=0; i < spFormLabels.length; i++) {
-	 	let label = spFormLabels[i].textContent;
-	 	label = label.trim();
-	 	label = label.replace(/[^a-zA-Z0-9]/g, '');
-	 	const row = spFormLabels[i].closest('tr');
-	 	row.id = 'row-' + label;
-	 	const dataCell = row.getElementsByClassName('ms-formbody')[0];
-	 	dataCell.id = 'data-' + label;
-	}
+const spFormLabels = document.querySelectorAll('.ms-formlabel');
+for (var i = 0; i < spFormLabels.length; i++) {
+    let label = spFormLabels[i].textContent;
+    label = label.trim();
+    label = label.replace(/[^a-zA-Z0-9]/g, '');
+    const row = spFormLabels[i].closest('tr');
+    row.id = 'row-' + label;
+    const dataCell = row.getElementsByClassName('ms-formbody')[0];
+    dataCell.id = 'data-' + label;
+}
