1f053bc4fb6fe4f88342eeca518cd49db802f45f
Andrea Goodson <5778606+akgd@users.noreply.github.com>
2018-05-17 17:50:09 +0100

    Delete script.js

--- Web part colors/script.js
+++
@@ -1,9 +0,0 @@
-//Requires jQuery. Add to content editor or HTML form web part.
-$( document ).ready(function() {
-//Green Web Part
-$('h3.ms-standardheader nobr span:contains("YOUR EXACT WP TITLE HERE")').closest(".s4-wpTopTable").addClass("green-web-part");
-//Yellow Web Part
-$('h3.ms-standardheader nobr span:contains("YOUR EXACT WP TITLE HERE")').closest(".s4-wpTopTable").addClass("yellow-web-part");
-//Force long WP Titles to wrap
-$('h3.ms-standardheader nobr span:contains("YOUR EXACT WP TITLE HERE")').closest(".s4-wpTopTable").addClass("long-wpHeader");
-});
