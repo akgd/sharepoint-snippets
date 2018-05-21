1b0e9f7ad0d2de17a1b87f9276696397c7432aad
Andrea Goodson <5778606+akgd@users.noreply.github.com>
2018-05-21 15:06:28 +0100

    Init

---
+++ editModeCheck.js
@@ -0,0 +1,6 @@
+var inDesignMode = document.forms[MSOWebPartPageFormName].MSOLayout_InDesignMode.value;
+if (inDesignMode == '1') {
+    //we are editing the page
+} else {
+   //we are not in edit mode
+}
