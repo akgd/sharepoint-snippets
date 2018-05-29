22c746771fb8453003fcf22223370e47055082db
Andrea Goodson <5778606+akgd@users.noreply.github.com>
2018-05-29 13:37:47 +0100

    Added option to keep existing parameters

--- redirectSpForm.js
+++ redirectSpForm.js
@@ -1,3 +1,9 @@
 var redirectUrl = 'YOUR URL';
 var sourceParam = '?&Source=' + encodeURIComponent(redirectUrl);
 window.history.pushState(null, null, sourceParam);
+
+//To keep existing parameters
+var redirectUrl = 'YOUR URL';
+var sourceParam = '?&Source=' + encodeURIComponent(redirectUrl);
+window.history.pushState(null, null, (window.location.href + sourceParam));
+
