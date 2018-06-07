f2e94806c50c90cef8a82c4d2340c25b81883990
Andrea Goodson <5778606+akgd@users.noreply.github.com>
2018-06-07 23:30:36 +0100

    Update and rename redirectSpForm.js to sp-form-add-redirect.js

--- sp-form-add-redirect.js
+++ sp-form-add-redirect.js
@@ -1,8 +1,9 @@
+//Add a redirect to the current url
 var redirectUrl = 'YOUR URL';
 var sourceParam = '?&Source=' + encodeURIComponent(redirectUrl);
 window.history.pushState(null, null, sourceParam);
 
-//To keep existing parameters
+//Use the below if there are existing parameters
 var redirectUrl = 'YOUR URL';
 var sourceParam = '?&Source=' + encodeURIComponent(redirectUrl);
 window.history.pushState(null, null, (window.location.href + sourceParam));
