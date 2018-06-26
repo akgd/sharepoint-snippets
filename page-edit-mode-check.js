408d794a4fe2abd1098a9feeb580e8e97f1d2a43
Andrea Goodson <5778606+akgd@users.noreply.github.com>
2018-06-27 00:10:50 +0100

    Update and rename page-edit-mode-check.js to pages-edit-mode-check.js

--- page-edit-mode-check.js
+++
@@ -1,6 +0,0 @@
-var inDesignMode = document.forms[MSOWebPartPageFormName].MSOLayout_InDesignMode.value;
-if (inDesignMode == '1') {
-    //we are in edit mode
-} else {
-   //we are not in edit mode
-}
---
+++ pages-edit-mode-check.js
@@ -0,0 +1,6 @@
+var inDesignMode = document.forms[MSOWebPartPageFormName].MSOLayout_InDesignMode.value; //Check to see if a web part page is in edit mode
+if (inDesignMode == '1') {
+    //page is being edited
+} else {
+   //page is not being edited
+}
