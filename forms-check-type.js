083c2050215bda3f14907a627ad04b2749aad95e
Andrea Goodson <5778606+akgd@users.noreply.github.com>
2018-06-26 23:57:20 +0100

    Rename

--- forms-check-type.js
+++ forms-check-type.js
@@ -1,18 +1,15 @@
-/* Use to determine the classic SP form type
- * the user is currently viewing. Run specific
- * functions based on whether it is a NewForm,
- * EditForm or DispForm. In the example below,
- * I add a class to the form based on the type,
- * and move the save and cancel buttons to a
+/* Use to determine the classic SP form type the user is currently 
+ * viewing. Run specific functions based on whether it is a NewForm,
+ * EditForm or DispForm. In the example below, I add a class to the 
+ * form based on the type and move the save and cancel buttons to a
  * custom element.
  */
- 
 
 //Check item form view
 function checkFormView(currentUrl) {
-    var newForm = (checkForSubstring(currentUrl, 'newform'));
-    var dispForm = (checkForSubstring(currentUrl, 'dispform'));
-    var editForm = (checkForSubstring(currentUrl, 'editform'));
+    var newForm = checkForSubstring(currentUrl, 'newform');
+    var dispForm = checkForSubstring(currentUrl, 'dispform');
+    var editForm = checkForSubstring(currentUrl, 'editform');
 
     if (newForm) {
         spNewForm();
@@ -22,7 +19,7 @@ function checkFormView(currentUrl) {
         spEditForm();
     } else {
         //console.log('Not a valid form URL.')
-    }    
+    }
 }
 
 //Check if string contains substring (not case sensitive)
@@ -40,15 +37,15 @@ function checkForSubstring(fullStr, subStr) {
 function spNewForm() {
     //Add class to our custom form div
     document.getElementById('root').classList.add('sp-new-form');
-    
+
     //move save button to our actions area
-    const saveBtn = document.querySelector('.ms-toolbar input[value="Save"]');
-    const saveEl = document.querySelector('#sp-form-save');
+    var saveBtn = document.querySelector('.ms-toolbar input[value="Save"]');
+    var saveEl = document.querySelector('#sp-form-save');
     saveEl.appendChild(saveBtn);
-    
+
     //move cancel button to our actions area
-    const cancelBtn = document.querySelector('.ms-toolbar input[value="Cancel"]');
-    const cancelEl = document.querySelector('#sp-form-cancel');
+    var cancelBtn = document.querySelector('.ms-toolbar input[value="Cancel"]');
+    var cancelEl = document.querySelector('#sp-form-cancel');
     saveEl.appendChild(saveBtn);
 }
 
@@ -62,14 +59,14 @@ function spDispForm() {
 function spEditForm() {
     //Add class to our custom form div
     document.getElementById('root').classList.add('sp-edit-form');
-    
+
     //move save button to our actions area
-    const saveBtn = document.querySelector('.ms-toolbar input[value="Save"]');
-    const saveEl = document.querySelector('#sp-form-save');
+    var saveBtn = document.querySelector('.ms-toolbar input[value="Save"]');
+    var saveEl = document.querySelector('#sp-form-save');
     saveEl.appendChild(saveBtn);
-    
+
     //move cancel button to our actions area
-    const cancelBtn = document.querySelector('.ms-toolbar input[value="Cancel"]');
-    const cancelEl = document.querySelector('#sp-form-cancel');
+    var cancelBtn = document.querySelector('.ms-toolbar input[value="Cancel"]');
+    var cancelEl = document.querySelector('#sp-form-cancel');
     saveEl.appendChild(saveBtn);
 }
