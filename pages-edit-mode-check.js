a5fbe56ba4b89caa7bfe3dd318aa201f9e7b32b8
Andrea Goodson <akgdev@icloud.com>
2019-05-28 09:38:20 +0100

    Adding example use

--- pages-edit-mode-check.js
+++ pages-edit-mode-check.js
@@ -3,5 +3,20 @@ var inDesignMode = document.forms[MSOWebPartPageFormName].MSOLayout_InDesignMode
 if (inDesignMode == '1') {
     // Page is being edited
 } else {
-   // Page is not being edited
+    // Page is not being edited
+}
+
+// Example of use case
+var inDesignMode = document.forms[MSOWebPartPageFormName].MSOLayout_InDesignMode.value;
+var spRibbon = document.getElementById('s4-ribbonrow');
+var spLeftNav = document.getElementById('sideNavBox');
+var spTitleRow = document.getElementById('s4-titlerow');
+if (inDesignMode == '1') {
+    // Page is being edited
+    spLeftNav.style.display = 'none';
+} else {
+    // Page is not being edited
+    spRibbon.style.display = 'none';
+    spLeftNav.style.display = 'none';
+    spTitleRow.style.display = 'none';
 }
