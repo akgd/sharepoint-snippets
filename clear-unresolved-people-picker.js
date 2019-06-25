a8034361694fef5e06995a7890858068bf27a96e
Andrea Goodson <akgdev@icloud.com>
2019-06-25 17:57:46 +0100

    Update clear-unresolved-people-picker.js

--- clear-unresolved-people-picker.js
+++ clear-unresolved-people-picker.js
@@ -1,20 +1,12 @@
 function clearUnresolvedPeople() {
-
     var peoplePickers = document.querySelectorAll('.sp-peoplepicker-topLevel');
-
     for (i = 0; i < peoplePickers.length; i++) {
-
         var picker = peoplePickers[i];
         var pickerInput = picker.querySelector('input[id$="_HiddenInput"]');
-        
         if (pickerInput.value) {
-
             var people = JSON.parse(pickerInput.value);
-
             for (p = 0; p < people.length; p++) {
-
                 if (people[p].Key === '') {
-
                     var userEl = picker.querySelector('.sp-peoplepicker-userSpan[sid=""]')
                     userEl.parentNode.removeChild(userEl);
                     people.splice(p, 1);
@@ -22,7 +14,6 @@ function clearUnresolvedPeople() {
 
                 }
             }
-
         }
     }
 }
