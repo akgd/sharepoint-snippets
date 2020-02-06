63749a0c4b2bbac51891098fbc9eb434bfbcd8d6
Andrea Goodson <akgdev@icloud.com>
2020-02-06 11:03:28 +0000

    Create custom-submit-button.js

---
+++ custom-submit-button.js
@@ -0,0 +1,20 @@
+    // Handle custom submit buttons
+    $('.sp-custom-save').click(function () {
+        var button = $(this);
+        // check if this is a double click
+        var disabledStatus = button.attr('disabled');
+        if (disabledStatus != 'disabled') {
+            // run submit process
+            customSubmit();
+        }
+        // disable el to prevent double click
+        button.attr('disabled', 'disabled');
+        // reactivate in case submission goes wrong
+        setTimeout(function () {
+            button.removeAttr('disabled');
+        }, 1500);
+    });
+
+    function customSubmit() {
+        $('.ms-toolbar input[value="Save"]').first().click();
+    }
